304abb2b9a6ff7cc03f5eef4692c53b4f640c2c6
FufitoP <carlos.murillo.badilla@est.una.ac.cr>
2017-09-14 00:52:22 -0600

    Main

---
+++ src/proyecto/PPIII.java
@@ -0,0 +1,16 @@
+package proyecto;
+
+import proyecto.controller.controller;
+import proyecto.model.model;
+import proyecto.view.view;
+
+public class PPIII {
+
+    public static void main(String[] args) {
+        model model=new model() ;
+        view view = new view();
+        controller controller = new controller(model,view);
+        view.setVisible(true);
+
+    }
+}
