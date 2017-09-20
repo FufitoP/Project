51b2424874e59553bcfeb1693889af056b8e23fb
FufitoP <carlos.murillo.badilla@est.una.ac.cr>
2017-09-19 22:08:14 -0600

    Main (probando el método de model.acepta())

--- src/proyecto/PPIII.java
+++ src/proyecto/PPIII.java
@@ -2,15 +2,29 @@ package proyecto;
 
 import proyecto.controller.controller;
 import proyecto.model.model;
+import proyecto.model.data;
+import proyecto.model.transicion;
 import proyecto.view.view;
 
 public class PPIII {
 
     public static void main(String[] args) {
-        model model=new model() ;
-        view view = new view();
-        controller controller = new controller(model,view);
-        view.setVisible(true);
-
+            model model = new model();
+            /*view view = new view();
+            controller controller = new controller(model,view);
+            view.setVisible(true);*/
+            
+            data d = new data("Data", 'F');
+            data da = new data("SDA", 'M');
+            data d2 = new data("PP", 'F');
+            data d3 = new data("r.", 'I');
+            transicion t = new transicion(da, d, "skj");
+            transicion t1 = new transicion(d2, d3, "uij");
+            
+            model.addTransiciones(t);
+            model.addTransiciones(t1);
+            
+            System.out.print(model.acepta("su"));
+            
     }
 }
